e7fb70a20010ee33707223d054333292dba1a5d2
hemprichbennett <hemprich.bennett@gmail.com>
2018-04-30 20:45:34 +0200

    Changed line which was causing the output df to just repeat the largest clustering level used, not the appropriate values of it

--- R/randomized_ranges.R
+++ R/randomized_ranges.R
@@ -43,7 +43,7 @@ randomized_ranges <- function(networks, indices, network_level = 'both', n_perm=
         #print(names(rand_list)[a])
         mat <- rbind(mat, t(sapply(rand_list[[a]], function(x) quantile(x, probs=quantiles_to_return))))
       }
-      mat <- cbind(rownames(mat), rep(names(rand_list)[a], nrow(mat)), rep(index_used, nrow(mat)), mat)
+      mat <- cbind(rownames(mat), rep(names(rand_list)[[a]], nrow(mat)), rep(index_used, nrow(mat)), mat)
       outmat <- rbind(outmat, mat) #Something in creating this isn't right, it gave me crappy values in the clustering column for a trial. I think the loop is wrong.
       outmat <- as.data.frame(outmat)
       for(d in 4:ncol(outmat)){
