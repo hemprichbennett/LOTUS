13fbd77528580257117eb4cabf0fe468906ebbdd
hemprichbennett <hemprich.bennett@gmail.com>
2018-05-01 11:17:31 +0100

    Renamed the output column names to standardise between metcalcs and randomized_ranges

--- R/metcalcs.R
+++ R/metcalcs.R
@@ -7,7 +7,7 @@ metcalcs <- function(lis, indexes, netlevel = 'both'){
 
   m <- reshape2::melt(out)
   m <- m[,-2] #column two is just a tonne of 1s from the lapply etc, discard it
-  colnames(m) <- c('metric', 'value', 'dataset', 'clustering')
+  colnames(m) <- c('metric', 'value', 'network', 'clustering')
   m$clustering <- as.numeric(m$clustering)
   m$metric <- gsub('_', ', ',m$metric)
   return(m)
