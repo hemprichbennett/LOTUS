40c38414a129bdff527a2fb29f0676a37581571a
hemprichbennett <hemprich.bennett@gmail.com>
2018-05-18 16:16:51 +0100

    Made helpfile for the batnets dataset

---
+++ R/batnets-data.R
@@ -0,0 +1,14 @@
+#' Network metabarcoding data from Texas and Malaysia
+#'
+#' Data from metabarcoding guano samples of insectivorous bats,
+#' using the Zeale (2011) primers. The data is organised as a nested list,
+#' with the first list level being the clustering level used, the second list level
+#' being the dataset in question, and the final objects being interaction matrixes
+#'
+#' @docType data
+#'
+#' @usage data(batnets)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
