5f8dfa52001787e82d8e8159165ae157379768d1
hemprichbennett <hemprich.bennett@gmail.com>
2018-05-21 10:33:04 +0100

    Created LOTUS package manual

---
+++ R/LOTUS.R
@@ -0,0 +1,7 @@
+#' @details
+#' LOTUS currently consists of two basic commands: \code{\link{metcalcs}} for comparing how network-level metrics vary
+#' over MOTU clustering thresholds, and \code{\link{line_plot}} for visualising the results.
+#' \code{\link{batnets}} is included as an example dataset.
+#' See the vignette for details
+#' @keywords internal
+"_PACKAGE"
