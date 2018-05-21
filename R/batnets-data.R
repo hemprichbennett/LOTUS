a2f0976cec924b777e4b2291156604e1bc6ef17f
hemprichbennett <hemprich.bennett@gmail.com>
2018-05-21 10:33:26 +0100

    Made batnets data manual

--- R/batnets-data.R
+++ R/batnets-data.R
@@ -1,14 +1,15 @@
-#' Network metabarcoding data from Texas and Malaysia
+#' A dataset of adjacency matrices of bat-insect interactions
 #'
 #' Data from metabarcoding guano samples of insectivorous bats,
 #' using the Zeale (2011) primers. The data is organised as a nested list,
 #' with the first list level being the clustering level used, the second list level
-#' being the dataset in question, and the final objects being interaction matrixes
+#' being the dataset in question, and the final objects being interaction matrixes.
+#' The data is from Big Bend National Park, Texas and the SAFE project, Malaysia.
 #'
-#' @docType data
+#' @references A large-scale forest fragmentation experiment: the Stability of Altered Forest Ecosystems Project
+#' Robert M. Ewers, Raphael K. Didham, Lenore Fahrig, Gonçalo Ferraz, Andy Hector, Robert D. Holt, Valerie Kapos, Glen Reynolds, Waidi Sinun, Jake L. Snaddon, Edgar C. Turner
+#' Phil. Trans. R. Soc. B 2011 366 3292-3302; DOI: 10.1098/rstb.2011.0049. Published 17 October 2011
 #'
-#' @usage data(batnets)
-#'
-#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
-#'
-#' @keywords datasets
+#' @format A nested list of adjacancy matrices
+
+"batnets"
