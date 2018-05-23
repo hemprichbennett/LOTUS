8a388110a72e879d0b01e484a58b4718a5915af9
hemprichbennett <hemprich.bennett@gmail.com>
2018-05-23 13:51:10 +0100

    Added ability to analyse only one network at a time

--- R/metcalcs.R
+++ R/metcalcs.R
@@ -3,7 +3,7 @@ metcalcs <- function(networks, indices, network_level = 'both', list_format = 'c
   #'
   #' Acts as a wrapper for bipartite's networklevel and computeModules functions, calculating the difference between your given network-level metrics for a series of networks and clustering thresholds
   #'
-  #' @param networks A nested list of networks
+  #' @param networks A list of networks
   #' @param indices A vector of indices to be calculated. See the functions networklevel and computeModules in package bipartite for details
   #' @param network_level The network level to analyse
   #' @param list_format The input must be a nested list of networks, with the levels either being clustering, then network identity ('clust_net'), or network identity,
@@ -18,13 +18,17 @@ metcalcs <- function(networks, indices, network_level = 'both', list_format = 'c
     ll <- lapply(ll, function(X) setNames(X[nms], nms))
     ll <- apply(do.call(rbind, ll), 2, as.list)
     lapply(ll, function(X) X[!sapply(X, is.null)])
-  }#Function sourced from https://stackoverflow.com/questions/12723800/how-to-return-a-error-message-in-r/12723837#12723837
+  }#Function sourced from https://stackoverflow.com/questions/15263146/revert-list-structure
 
-  if(!list_format %in% c('clust_net', 'net_clust')){
-    stop("list_format must either be \'clust_net\' or \'net_clust\'")
+  #Set things up
+  if(!list_format %in% c('clust_net', 'net_clust', 'clust_only')){
+    stop("list_format must either be \'clust_net\', \'net_clust\' or \'clust_only\'")
   }
   if(list_format=='net_clust'){ #standardise the format
     networks <- list_rev(networks)
+  }
+  if(list_format=='clust_only'){
+    networks <- list(networks)
   }
 
   out <- list()
@@ -32,26 +36,55 @@ metcalcs <- function(networks, indices, network_level = 'both', list_format = 'c
   if('modularity' %in% indices){ #Modularity-only calculating session, as it uses a different command in bipartite to all other metrics
     indices <- indices[-which(indices=='modularity')]
     modularity = T
-    for(i in 1:length(networks)){
-      out[[names(networks)[i]]] <- lapply(networks[[i]], function(x) slot(bipartite::computeModules(web = x), 'likelihood'))
+    if(list_format == 'clust_only'){
+      for(i in 1:length(networks)){
+        out[[i]] <- lapply(networks[[i]], function(x) slot(bipartite::computeModules(web = x), 'likelihood'))
+      }
+      mods <- reshape2::melt(out)
+      mods <- cbind(mods, rep('modularity', nrow(mods)))
+      mods <- mods[-3]# This value is meaningless if theres no network identity to work with
+      colnames(mods) <- c('value', 'clustering', 'metric')
+      mods <- mods[,c(3,1,2)] #Reordering the columns to keep it uniform with the networklevel outputs
+      mods$clustering <- as.numeric(mods$clustering)
     }
-    mods <- reshape2::melt(out)
-    mods <- cbind(mods, rep('modularity', nrow(mods)))
-    colnames(mods) <- c('value', 'network', 'clustering', 'metric')
-    mods <- mods[,c(4,1,2,3)] #Reordering the columns to keep it uniform with the networklevel outputs
-    mods$clustering <- as.numeric(mods$clustering)
+    if(list_format != 'clust_only'){
+      for(i in 1:length(networks)){
+        out[[names(networks)[i]]] <- lapply(networks[[i]], function(x) slot(bipartite::computeModules(web = x), 'likelihood'))
+      }
+      mods <- reshape2::melt(out)
+      mods <- cbind(mods, rep('modularity', nrow(mods)))
+      colnames(mods) <- c('value', 'network', 'clustering', 'metric')
+      mods <- mods[,c(4,1,2,3)] #Reordering the columns to keep it uniform with the networklevel outputs
+      mods$clustering <- as.numeric(mods$clustering)
+    }
+
+
   }
+
+
   if(length(indices>0)){ #The rest of the metrics
-    for(i in 1:length(networks)){
-      out[[names(networks)[i]]] <- lapply(networks[[i]], function(x) as.matrix(bipartite::networklevel(web = x, index = indices, level = network_level)))
+    if(list_format =='clust_only'){
+        for(i in 1:length(networks)){
+          out[[i]] <- lapply(networks[[i]], function(x) as.matrix(bipartite::networklevel(web = x, index = indices, level = network_level)))
+        }
+      m <- reshape2::melt(out)
+      m <- m[-c(2,5)]# This value is meaningless if theres no network identity to work with
+      colnames(m) <- c('metric',  'value', 'clustering')
+      m$clustering <- as.numeric(m$clustering)
+
+      }
+    if(list_format != 'clust_only'){
+      for(i in 1:length(networks)){
+        out[[names(networks)[i]]] <- lapply(networks[[i]], function(x) as.matrix(bipartite::networklevel(web = x, index = indices, level = network_level)))
+      }
+      m <- reshape2::melt(out)
+      m <- m[,-2] #column two is just a tonne of 1s from the lapply etc, discard it
+      colnames(m) <- c('metric', 'value', 'network', 'clustering')
+      m$clustering <- as.numeric(m$clustering)
+      m$metric <- gsub('_', ', ',m$metric)
+      }
 
     }
-    m <- reshape2::melt(out)
-    m <- m[,-2] #column two is just a tonne of 1s from the lapply etc, discard it
-    colnames(m) <- c('metric', 'value', 'network', 'clustering')
-    m$clustering <- as.numeric(m$clustering)
-    m$metric <- gsub('_', ', ',m$metric)
-  }
 
 
 if(modularity ==T & length(indices>0)){
